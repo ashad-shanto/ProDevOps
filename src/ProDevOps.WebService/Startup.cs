6894b29db851658ff9c10c1a8a363eadf33ed784
Md Ashaduzzaman <md.ashaduzzaman@selise.ch>
2019-11-26 21:01:12 +0600

    revert test, project version to 2.1

--- src/ProDevOps.WebService/Startup.cs
+++ src/ProDevOps.WebService/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace ProDevOps.WebService
 {
@@ -16,22 +15,19 @@ namespace ProDevOps.WebService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseMvc(routes =>
+            app.Run(async (context) =>
             {
-                routes.MapRoute("default", "ProDevOps/{controller}/{action}/{id?}");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
