9e89270bc9caf84be37691397281389fe572f029
Md Ashaduzzaman <md.ashaduzzaman@selise.ch>
2019-12-04 18:30:13 +0600

    Fix unit test

--- src/ProDevOps.WebService/Startup.cs
+++ src/ProDevOps.WebService/Startup.cs
@@ -27,7 +27,7 @@ namespace ProDevOps.WebService
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! ProDevOps CI/CD 7-dev-merge-master");
+                await context.Response.WriteAsync("Hello World! ProDevOps CI/CD 8-dev-merge-master");
             });
         }
     }
